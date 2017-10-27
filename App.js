a1c46268419b2fc64d4544709aef42059b42bb1a
Hieu Phan <hieu.phq@dwarvesf.com>
2017-10-27 15:26:31 +0700

    Init react native project with reasonml and mobx

---
+++ App.js
@@ -0,0 +1,9 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React from 'react';
+import { app } from './lib/js/re/app.js';
+export default app;
